02be5c7c0b7ced26d12031d611e7daa1db35481b
oomiya karen <krn971138@gmail.com>
2021-04-07 23:01:00 +0900

    new,index実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # newで入力した値を@postに代入
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :content)
   end
 end
