7a7a9664d30bc1bbbcb4c62e33e1cc104e0000ca
oomiya karen <krn971138@gmail.com>
2021-04-10 02:54:02 +0900

    post実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_params, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -8,31 +9,47 @@ class PostsController < ApplicationController
   end
 
   def create
-    # newで入力した値を@postに代入
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to root_path
-    else
+    if params[:back]
       render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
     end
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
   def update
+    if @post.update(post_params)
+      redirect_to root_path, notice: "編集しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_path, notice: "削除しました"
   end
 
-
-
   private
+  def set_params
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:image, :content, :image_cache)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
   root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
