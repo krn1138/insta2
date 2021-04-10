0fcb205fdf48ae72271c37ba8b3eab880f632a8f
oomiya karen <krn971138@gmail.com>
2021-04-11 00:46:43 +0900

    バリデーション、ユニーク制約設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 20 }
+    validates :email, presence: true, length: { maximum: 30 }
+                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                      before_validation { email.downcase! }
 end
