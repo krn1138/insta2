a73f9c9d50d695d1a0aabc58bce87d44d281dca7
oomiya karen <krn971138@gmail.com>
2021-04-16 02:29:39 +0900

    4/15

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    include SessionsHelper
     # def not_sign_in
     #   unless logged_in?
     #     redirect_to new_user_path, notice: "新規登録してください"
